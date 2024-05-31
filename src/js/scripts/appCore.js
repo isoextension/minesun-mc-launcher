e5fa16dcd327f90c25c86f53af5a159cf7d0f9f1
isoextension <163932813+isoextension@users.noreply.github.com>
2024-05-31 16:34:56 -0300

    Add files via upload

---
+++ src/js/scripts/appCore.js
@@ -0,0 +1,32 @@
+const { IpcRenderer, shell, webFrame } = require('electron');
+
+// window onload preload
+window.onload = () => {
+    let preloader = document.getElementById('preloader');
+
+    preloader.style.visibility = 'hidden';
+    preloader.style.opacity = '0';
+}
+
+document.addEventListener('readystatechange', () => {
+    if (document.readyState === 'interactive') {
+        const remote = require('@electron/remote');
+
+        document.getElementById('frameBtn_close').addEventListener('click', () => {
+            const windows = remote.getCurrentWindow();
+            windows.close();
+        })
+        document.getElementById('frameBtn_minimize').addEventListener('click', () => {
+            const windows = remote.getCurrentWindow();
+            windows.minimize();
+        })
+    }
+})
+
+// Open external links in default browser
+document.addEventListener('click', (e) => {   
+    if (e.target.tagName === 'A' && e.target.href.startsWith('http')) {
+        e.preventDefault();
+        shell.openExternal(e.target.href);
+    }
+})
